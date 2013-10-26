78d673d593a56889817300d45cd5bf67bfaffd78
Fenexomega <jordyfgomes@gmail.com>
2013-10-26 11:37:25 -0300

    Initializing the project

---
+++ Openerraria/drawableGameComponent.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Openaria
+{
+	public class drawableGameComponent
+	{
+		Texture2D sprite;
+		public drawableGameComponent (ContentManager Content)
+		{
+		}
+	}
+}
+
